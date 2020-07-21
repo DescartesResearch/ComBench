6b979c0f2522bcc1b4d58ba407250e3becce0f8d
s353581 <maximilian.leidinger@stud-mail.uni-wuerzburg.de>
2020-07-21 10:48:26 +0200

    test workflows

--- Client/app/tests.py
+++ Client/app/tests.py
@@ -96,6 +96,7 @@ class TestController(unittest.TestCase):
         # checks if the process command is called
         self.assertTrue(mock_subproc_popen.called)
 
+
     @mock.patch('subprocess.Popen')
     def test_check_ntp_synchronization_enabled(self, mock_subproc_popen):
         # TODO check "check_ntp_synchronization" by mocking
